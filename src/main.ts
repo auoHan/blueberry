b63313dfb8ba548dce736feba305440bab3f5829
auoHan <710543268@qq.com>
2021-11-20 09:41:03 +0800

    修改Nav组件的名字为Footer

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import Nav from '@/components/Nav.vue';
+import Nav from '@/components/Footer.vue';
 import Layout from '@/components/Layout.vue';
 import Icon from '@/components/Icon.vue';
 
