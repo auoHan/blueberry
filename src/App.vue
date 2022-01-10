386213119de49706ccfadaced30e263046f9eeeb
auoHan <710543268@qq.com>
2022-01-10 09:02:39 +0800

    设置页面最大宽度

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,18 @@
 <style lang="scss">
 @import "~@/assets/style/reset.scss";
 @import "~@/assets/style/helper.scss";
-body{
+
+body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #333;
   font-family: $font-hei;
   line-height: 1.5;
   font-size: 16px;
+}
+
+#app {
+  max-width: 500px;
+  margin: 0 auto;
 }
 </style>
